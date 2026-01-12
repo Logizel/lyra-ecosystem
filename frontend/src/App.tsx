4d7b15b337ea1b93764a434a63470d14b2e4f8ed
LogiZel <jizelprince@gmail.com>
2026-01-13 03:02:17 +0530

    Initial Folder Structure and Backend containerized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App(){
+return(
+<div 
+      style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems: "center",
+      height:"100vh",
+      }}
+    >
+    <h1>Welcome to Lyra</h1>
+    </div>
+);
+}
